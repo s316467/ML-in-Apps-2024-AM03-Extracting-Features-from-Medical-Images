fc215e5c515961b7a5b4ca1ca890ca5b447fd889
Gardusio <fabiobarbieri.dev@gmail.com>
2024-05-24 16:34:06 +0200

    adds storing baseline latents

--- EFMI/baseline/resnet.py
+++ EFMI/baseline/resnet.py
@@ -6,6 +6,7 @@ from sklearn import svm
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 
+
 class Resnet50Extractor:
     def __init__(self):
         self.resnet50 = resnet50(weights=ResNet50_Weights.IMAGENET1K_V2)
@@ -14,21 +15,22 @@ class Resnet50Extractor:
             param.requires_grad = False
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         self.resnet50 = self.resnet50.to(self.device)
-        
+
     def extract_features(self, data_loader):
         self.resnet50.eval()
         features = []
         labels = []
         with torch.no_grad():
-            for image, label, patient_id, _  in data_loader:
+            for image, label, patient_id, _ in data_loader:
                 image = image.to(self.device)
                 features_batch = self.resnet50(image).squeeze()
                 features.append(features_batch.cpu().numpy())
                 labels.append(label.numpy())
-                print("Feature extracted for image: ", patient_id)
         features = np.concatenate(features)
         labels = np.concatenate(labels)
         print(features.shape)
+        np.save(features, "resnet50_features")
         print(labels.shape)
-        
+        np.save(labels, "resnet50_labels")
+
         return features, labels
