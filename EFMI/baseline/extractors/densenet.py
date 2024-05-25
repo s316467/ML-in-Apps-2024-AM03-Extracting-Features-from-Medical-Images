ce8069fe37c581b57805f0a7b5bbffb5c174e7bc
Gardusio <fabiobarbieri.dev@gmail.com>
2024-05-25 16:19:38 +0000

    WIP adds densenet121 feature extractor

--- EFMI/baseline/extractors/densenet.py
+++ EFMI/baseline/extractors/densenet.py
@@ -8,7 +8,7 @@ class DensenetExtractor(BaselineExtractor):
     def __init__(self, latent_dim):
         super().__init__(
             model=densenet121(weights=DenseNet121_Weights.DEFAULT),
-            reduction_layer=nn.Linear(1024 * 7 * 7, latent_dim),
+            reduction_layer=nn.Linear(1024 * 16 * 16, latent_dim),
             model_name="Densenet121",
             latent_dim=latent_dim,
         )
