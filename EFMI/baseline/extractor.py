4ea907385e8cc40e74f089b65fb073a635b57bdb
Gardusio <fabiobarbieri.dev@gmail.com>
2024-05-30 12:15:48 +0200

    moves baseline results in a dedicated baseline folder

--- EFMI/baseline/extractor.py
+++ EFMI/baseline/extractor.py
@@ -45,7 +45,7 @@ class BaselineExtractor:
         labels = np.concatenate(labels)
 
         print("Saving features as numpy arrays...")
-        np.save(f"{self.model_name}_features_{self.latent_dim}.npy", features)
-        np.save(f"{self.model_name}_labels_{self.latent_dim}.npy", labels)
+        np.save(f"./results/{self.model_name}_features_{self.latent_dim}.npy", features)
+        np.save(f"./results/{self.model_name}_labels_{self.latent_dim}.npy", labels)
 
         return features, labels
